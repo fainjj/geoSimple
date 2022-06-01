c01906fd98598cd5bc6b99b4fb1ae098e8f3eb44
Justin Fain <jstnfain@gmail.com>
2022-06-01 11:48:01 -0400

    Bring gs.read.detect into naming scheme

---
+++ Code/gs.read.detect.R
@@ -0,0 +1,8 @@
+gs.read.detect <- function(fpath) {
+  switch (fpath,
+    grepl('\\.shp', fpath) = sf::st_read(fpath),
+    grepl('\\.xlsx', fpath) = readxl::read_xlsx(fpath),
+    grepl('\\.xls', fpath) = readxl::read_xls(fpath),
+    grepl('\\.csv', fpath) = readr::read_csv(fpath)
+  )
+}
